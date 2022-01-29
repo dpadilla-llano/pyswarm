cee263abf193597813561a257ecb52bb5805733c
dpadilla-llano <98504807+dpadilla-llano@users.noreply.github.com>
2022-01-29 15:51:00 -0500

    Update __init__.py

--- pyswarm/__init__.py
+++ pyswarm/__init__.py
@@ -1,17 +1,23 @@
 """
 ================================================================================
-pyswarm: Particle swarm optimization (PSO) with constraint support
+pyaspso: Particle Swarm Optimization with adaptive  
 ================================================================================
 
-Authors: Abraham Lee
-         Sebastian Castillo-Hair
-Copyright: 2013-2015
+Author: David Padilla-Llano, Ph.D in Structural Engineer and Research Scientist 
+(Copyright 2022)
+
+About: This code is derived from the original *pysarm* @tisimst/pyswarm package 
+by Abraham Lee and Sebastian Castillo-Hair (Copyright: 2013-2015).
+
+================================================================================
+pyswarm: Particle swarm optimization (PSO) with constraint support
+================================================================================
 
 """
 # from __future__ import absolute_import
 
-__author__ = 'Abraham Lee'
-__version__ = '0.7'
+__author__ = 'David Padilla-Llano'
+__version__ = '1.0'
 
 from pyswarm.pso import *
     
